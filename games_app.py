903b07b494dc2ea73d4f0d8be8635e6b2cadba30
xtelos <df4182@gmail.com>
2023-11-18 13:04:19 -0500

    added functionality for search bar

--- games_app.py
+++ games_app.py
@@ -6,18 +6,19 @@ import redis
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
 
 
-@app.route('/games/<name>')
-def root(name):
+@app.route('/games/<name>', methods=['GET', 'POST'])
+def get_game(name):
     r = redis.Redis(host='localhost', port=6379, decode_responses=True)
     gamePrice = r.hget(name, "steam_price")
     return jsonify({'name': name,
                     'price': gamePrice})
 
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
