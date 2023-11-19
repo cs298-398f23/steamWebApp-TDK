0c1522878de91e4cd6bcd28273ea004c75ce5192
Kyle <adamsk04@moravian.edu>
2023-11-19 17:04:17 -0500

    Updated scrape_steam.py

--- scrape_steam.py
+++ scrape_steam.py
@@ -3,11 +3,11 @@
 import requests
 import redis
 
-def get_game_id(game_name):
-    params = {"term": game_name, "l": "english", "cc": "us"}
-    request = requests.get("http://store.steampowered.com/api/storesearch/", params=params)
+def get_steam_games():
+    params = {"l": "english", "cc": "us"}
+    request = requests.get("http://api.steampowered.com/ISteamApps/GetAppList/v0002/", params=params)
     json = request.json()
-    return json["items"][0]["id"]
+    return json["applist"]["apps"]
 
 def get_game_price(game_id):
     params = {"appids": game_id, "cc": "us", "filters": "price_overview"}
@@ -15,11 +15,22 @@ def get_game_price(game_id):
     json = request.json()
     return json[str(game_id)]["data"]["price_overview"]["final_formatted"]
 
-if __name__ == "__main__":
-    r = redis.Redis(host='localhost', port=6379, decode_responses=True)
-    game_name = "terraria"
-    game_id = get_game_id(game_name)
-    game_price = get_game_price(game_id)
+def add_game_to_redis(game_name, game_price, r):
     r.hset(game_name, mapping={
         "steam_price": game_price}
         )
+
+if __name__ == "__main__":
+    count = 1
+    games = get_steam_games()
+    r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+    for game in games:
+        if game["name"] != "":
+            try: 
+                game_price = get_game_price(game["appid"])
+                add_game_to_redis(game["name"].lower(), game_price, r)
+            except Exception as e:
+                pass
+            print(count)
+            count += 1
+    
